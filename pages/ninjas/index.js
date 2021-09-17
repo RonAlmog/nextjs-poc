16e93c29ccb076a658c2b4fb33f97ef4554c41b4
Ron Almog <ron8132@gmail.com>
2021-09-16 23:14:55 -0400

    fetch static data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,27 @@
-const Ninjas = () => {
-  return <div>Ninjas</div>;
+import styles from "../../styles/Ninjas.module.css";
+
+// run on build time only, not in browser
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Ninjas;
